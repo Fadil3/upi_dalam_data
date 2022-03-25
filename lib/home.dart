06f31073bad87c123efd21fd01515e10a72045b5
Muhammad Rayhan Fadillah <mrayhanfadil@upi.edu>
2022-03-25 18:49:54 +0700

    split card to another file

---
+++ lib/home.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+// function to create the card
+Widget cardData() {
+  return SingleChildScrollView(
+      child: Row(
+        // center
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+      SizedBox(
+        width: 450,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children:[
+            Card(
+            child:Column(
+              children: [
+                Image.network(
+              'https://images.unsplash.com/photo-1524178232363-1fb2b075b655?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
+              fit: BoxFit.cover,
+            ),
+                ListTile(
+                  title: const Text('Data Dosen'),
+                  subtitle: Text('Penelitian, Pengabdian, dan Pengajaran',style: TextStyle(color: Colors.black.withOpacity(0.6)),),
+                  leading: const Icon(Icons.supervisor_account_outlined),
+                ),
+                ButtonBar(
+                  alignment: MainAxisAlignment.center,
+                  children :[ TextButton.icon(
+                    icon: const Text('Lihat'),
+                    label: const Icon(Icons.arrow_forward_rounded),
+                    onPressed: () {
+                      /* ... */
+                      },
+                    ),
+                  ]
+                ),
+              ],
+            )
+          ),
+          const Padding(padding: EdgeInsets.all(8.0)),
+          ]
+        ),
+      ),
+        ],
+      ) 
+    );
+}
