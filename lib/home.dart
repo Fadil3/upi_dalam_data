53ff9ffc0bcadbfc266440261937c4efc3eaae51
Muhammad Rayhan Fadillah <mrayhanfadil@upi.edu>
2022-03-30 06:57:12 +0700

    add more card

--- lib/home.dart
+++ lib/home.dart
@@ -75,7 +75,7 @@ Widget home() {
               fit: BoxFit.cover,
             ),
                 ListTile(
-                  title: const Text('Data Dosen'),
+                  title: const Text('Data Program Studi'),
                   subtitle: Text('Penelitian, Pengabdian, dan Pengajaran',style: TextStyle(color: Colors.black.withOpacity(0.6)),),
                   leading: const Icon(Icons.supervisor_account_outlined),
                 ),
@@ -101,7 +101,54 @@ Widget home() {
         ),
           ],
         ),
-        ],
+        Column(
+          // center
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+          Row(
+          // center
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+        SizedBox(
+        width: 450,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children:[
+            Card(
+            child:Column(
+              children: [
+                Image.network(
+              'https://images.unsplash.com/photo-1524178232363-1fb2b075b655?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
+              fit: BoxFit.cover,
+            ),
+                ListTile(
+                  title: const Text('Data Mahasiswa'),
+                  subtitle: Text('Penelitian, Pengabdian, dan Pengajaran',style: TextStyle(color: Colors.black.withOpacity(0.6)),),
+                  leading: const Icon(Icons.supervisor_account_outlined),
+                ),
+                ButtonBar(
+                  alignment: MainAxisAlignment.center,
+                  children :[ TextButton.icon(
+                    icon: const Text('Lihat'),
+                    label: const Icon(Icons.arrow_forward_rounded),
+                    onPressed: () {
+                      /* ... */
+                      },
+                    ),
+                  ]
+                ),
+              ],
+            )
+          ),
+          const Padding(padding: EdgeInsets.all(8.0)),
+          ]
+        ),
+      ),
+          ],
+        ),
+          ],
+        ),
+      ],
     ) 
   );
 }
