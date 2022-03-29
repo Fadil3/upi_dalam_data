eb61c04be85a3495285b881f09c26ebedc43f1f1
Muhammad Rayhan Fadillah <mrayhanfadil@upi.edu>
2022-03-29 21:13:29 +0700

    create login class

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,39 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: _title,
-      home: MyStatefulWidget(),
+      home: LoginPage(),
+    );
+  }
+}
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text('Silahkan Login'),
+            ElevatedButton(
+              child: const Text('Login'),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => MyStatefulWidget()),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
