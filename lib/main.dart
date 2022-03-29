94169f919a9bcb58b9e211ed81c7ce5cc2140c94
Muhammad Rayhan Fadillah <mrayhanfadil@upi.edu>
2022-03-29 21:38:51 +0700

    add form nim

--- lib/main.dart
+++ lib/main.dart
@@ -35,8 +35,22 @@ class _LoginPageState extends State<LoginPage> {
             const Padding(
               padding: EdgeInsets.all(20),
             ),
-            const Text('Single Sign-on',
-                style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold)),
+            const Text('Single Sign-on',style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold)),
+            const Padding(padding: EdgeInsets.all(10),),
+            SizedBox(
+              width: 300,
+              child:TextFormField(
+                decoration:const InputDecoration(
+                  labelText: 'NIM / NIP / NIDN',
+                  errorText: 'Error message',
+                  border: OutlineInputBorder(),
+                  suffixIcon: Icon(
+                    Icons.error,
+                  ),
+                ),
+              ),
+            ),
+            
             ElevatedButton(
               child: const Text('Login'),
               onPressed: () {
