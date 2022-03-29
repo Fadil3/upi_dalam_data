ed73c6e5a889602e7a378b2b432731ecc6ab4739
Muhammad Rayhan Fadillah <mrayhanfadil@upi.edu>
2022-03-29 22:10:58 +0700

    add validation for user id

--- lib/main.dart
+++ lib/main.dart
@@ -25,35 +25,67 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final userIdController = TextEditingController();
+  final passwordController = TextEditingController();
+  dynamic userID;
+  dynamic password;
+
+  String _errorMessageUser = '';
+  String _errorMessagePassword = '';
+
+  @override
+  void dispose() {
+    userIdController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: Column(
           children: <Widget>[
-            Image.asset("images/logo-upi.png",height: 200,width: 200,),
+            Image.asset(
+              "images/logo-upi.png",
+              height: 200,
+              width: 200,
+            ),
             const Padding(
               padding: EdgeInsets.all(20),
             ),
-            const Text('Single Sign-on',style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold)),
-            const Padding(padding: EdgeInsets.all(10),),
+            const Text('Single Sign-on',
+                style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold)),
+            const Padding(
+              padding: EdgeInsets.all(10),
+            ),
             SizedBox(
               width: 300,
-              child:TextFormField(
-                decoration:const InputDecoration(
+              child: TextFormField(
+                controller: userIdController,
+                decoration: InputDecoration(
                   labelText: 'NIM / NIP / NIDN',
-                  errorText: 'Error message',
-                  border: OutlineInputBorder(),
-                  suffixIcon: Icon(
+                  errorText: _errorMessageUser == "" ? null : _errorMessageUser,
+                  border: const OutlineInputBorder(),
+                  suffixIcon: _errorMessageUser != "" ? const Icon(
                     Icons.error,
-                  ),
+                  ) : null,
                 ),
               ),
             ),
-            
             ElevatedButton(
               child: const Text('Login'),
               onPressed: () {
+                if (userIdController.text.isEmpty) {
+                  setState(() {
+                    _errorMessageUser = 'User ID cannot be empty';
+                  });
+                  return;
+                } else {
+                  setState(() {
+                    _errorMessageUser = '';
+                  });
+                }
                 Navigator.push(
                   context,
                   MaterialPageRoute(builder: (context) => const HomePage()),
@@ -66,4 +98,3 @@ class _LoginPageState extends State<LoginPage> {
     );
   }
 }
-
