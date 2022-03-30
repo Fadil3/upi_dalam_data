fabe61f3ad8c254444f8d5eb685cb03d620bfb9b
bimantoroar <75794187+bimantoroar@users.noreply.github.com>
2022-03-30 19:35:30 +0700

    update mahasiswa.dart
    menambah kan logo upi, card, button, dan navbar tetapi masih belum berfungsi navbar dan button nya

--- lib/mahasiswa.dart
+++ lib/mahasiswa.dart
@@ -1,4 +1,7 @@
 import "package:flutter/material.dart";
+import 'package:upi_dalam_data/contact.dart';
+import "package:upi_dalam_data/home.dart";
+import 'package:upi_dalam_data/setting.dart';
 
 class Mahasiswa extends StatefulWidget {
   const Mahasiswa({Key? key}) : super(key: key);
@@ -8,6 +11,19 @@ class Mahasiswa extends StatefulWidget {
 }
 
 class _MahasiswaState extends State<Mahasiswa> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  final List<Widget> _widgetOptions = <Widget>[
+    const Home(),
+    const Contact(),
+    const Setting(),
+  ];
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,7 +32,97 @@ class _MahasiswaState extends State<Mahasiswa> {
         title: const Text('Mahasiswa'),
       ),
       body: Center(
-        child: const Text("ini halaman Mahasiswa"),
+        child: Column(children: <Widget>[
+          Image.asset(
+            "images/logo-upi.png",
+            height: 50,
+            width: 50,
+          ),
+          SizedBox(
+            width: 300,
+            height: 50,
+            child: Card(
+                elevation: 2,
+                child: Text(
+                  "Jumlah Mahasiwa Pendidikan Indonesia",
+                  textAlign: TextAlign.center,
+                )),
+          ),
+          const Padding(
+            padding: EdgeInsets.all(20),
+          ),
+          Container(
+            width: 300,
+            height: 35,
+            child: ElevatedButton(
+              onPressed: () {
+                // Respond to button press
+              },
+              child: Text('DATA MAHASISWA BELUM BAYAR UKT'),
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.all(20),
+          ),
+          Container(
+            width: 300,
+            height: 35,
+            child: ElevatedButton(
+              onPressed: () {
+                // Respond to button press
+              },
+              child: Text('RATA-RATA LAMA STUDI'),
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.all(20),
+          ),
+          Container(
+            width: 300,
+            height: 35,
+            child: ElevatedButton(
+              onPressed: () {
+                // Respond to button press
+              },
+              child: Text('MAHASISWA PROGRAM DI LUAR KAMPUS'),
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.all(20),
+          ),
+          Container(
+            width: 300,
+            height: 35,
+            child: ElevatedButton(
+              onPressed: () {
+                // Respond to button press
+              },
+              child: Text('DATA PENGAJUAN BEASISWA'),
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.all(20),
+          ),
+        ]),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.contacts),
+            label: 'Contact',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Setting',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.teal[400],
+        onTap: _onItemTapped,
       ),
     );
   }
