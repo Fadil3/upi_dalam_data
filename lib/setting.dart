1abba39093d476ac35c2dd2b9aa8575b7631592a
dennisnuriman <dennismuhammad.md@gmail.com>
2022-03-30 19:10:49 +0700

    nama
    di bawah foto di tambah nama, nim, dan prodi

--- lib/setting.dart
+++ lib/setting.dart
@@ -5,13 +5,31 @@ import 'package:flutter/material.dart';
 Widget settingPage(){
   return Column(
     children: [
+
+      Padding(
+        padding: EdgeInsets.all(20)
+      ),
+
       SizedBox(
         height: 115,
         width: 115,
         child: CircleAvatar(
           backgroundImage: NetworkImage("https://assets.pikiran-rakyat.com/crop/0x0:0x0/x/photo/2022/01/21/992560672.png"),
         ),
-      )
+      ),
+
+      Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Text(
+          'Asep Knalpot\n1900141\nIlmu Komputer',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 18,
+          ),
+          textAlign: TextAlign.center,
+          ),
+      ),
+
     ],
   );
 }
