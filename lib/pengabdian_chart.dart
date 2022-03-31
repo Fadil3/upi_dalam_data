8468336cc809279140d35c598c32d2c17e7fd789
Muhammad Rayhan Fadillah <mrayhanfadil@upi.edu>
2022-03-31 20:16:40 +0700

    fix warning

--- lib/pengabdian_chart.dart
+++ lib/pengabdian_chart.dart
@@ -13,21 +13,21 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: '_title',
-      home: MyStatefulWidget(),
+    return const MaterialApp(
+      title: "Chart",
+      home: ChartPengabdian(),
     );
   }
 }
 
-class MyStatefulWidget extends StatefulWidget {
-  const MyStatefulWidget({Key? key}) : super(key: key);
+class ChartPengabdian extends StatefulWidget {
+  const ChartPengabdian({Key? key}) : super(key: key);
 
   @override
-  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+  State<ChartPengabdian> createState() => _ChartPengabdianState();
 }
 
-class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+class _ChartPengabdianState extends State<ChartPengabdian> {
   late List<GDPData> _chartData;
   late TooltipBehavior _tooltipBehavior;
 
@@ -38,23 +38,12 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
     super.initState();
   }
 
-  int _selectedIndex = 0;
-  static const TextStyle optionStyle =
-      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-  final List<Widget> _widgetOptions = <Widget>[];
-
   List<GDPData> getChartData() {
     final List<GDPData> chartData = [
       GDPData('Yang sudah', 1600),
       GDPData('Yang belum', 2490),
     ];
     return chartData;
-  }
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
   }
 
   @override
@@ -71,37 +60,10 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
             dataSource: _chartData,
             xValueMapper: (GDPData data, _) => data.continent,
             yValueMapper: (GDPData data, _) => data.gdp, // PieSeries
-            dataLabelSettings: DataLabelSettings(isVisible: true),
+            dataLabelSettings: const  DataLabelSettings(isVisible: true),
             enableTooltip: true)
       ],
     )));
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('BottomNavigationBar Sample'),
-      ),
-      body: Center(
-        child: _widgetOptions.elementAt(_selectedIndex),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.contacts),
-            label: 'Contact',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.settings),
-            label: 'Setting',
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.teal[400],
-        onTap: _onItemTapped,
-      ),
-    );
   }
 }
 
