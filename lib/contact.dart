6b2d0bfcf0f4e8fe6db2576b7ead3e93c6864fd1
dennisnuriman <dennismuhammad.md@gmail.com>
2022-04-01 00:51:50 +0700

    pimpinan
    membuat dropdown untuk pimpinan yang berisi rektor dan wakil rektor

--- lib/contact.dart
+++ lib/contact.dart
@@ -8,13 +8,60 @@ class Contact extends StatefulWidget {
 }
 
 class _ContactState extends State<Contact> {
+  List<String> items1 = <String>[
+    'PIMPINAN UNIVERSITAS',
+    'Rektor',
+    'Wakil Rektor',
+  ];
+
+  String dropdownValue1 = 'PIMPINAN UNIVERSITAS';
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context){
     return Scaffold(
       body: Center(
-        child: const Text("ini halaman Contact"),
-      ),
+        child:
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      width: 300,
+                      padding: EdgeInsets.symmetric(horizontal: 12, vertical: 4),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(12),
+                          border: Border.all(
+                              color: Color.fromARGB(255, 96, 96, 96), width: 1)),
+                      child: DropdownButton<String>(
+                        onChanged: (String? newValue1) {
+                          setState(() {
+                            dropdownValue1 = newValue1!;
+                          });
+                        },
+                        value: dropdownValue1,
+                        items: items1.map<DropdownMenuItem<String>>(
+                          (String value1) {
+                            return DropdownMenuItem<String>(
+                              value: value1,
+                              child: Text(value1),
+                            );
+                          },
+                        ).toList(),
+                      ),
+                    ),
+                  ],
+                ),
+              const Padding(padding: EdgeInsets.all(10.0)),
+              ],
+            ),
+          ],
+        ),        
+      )
     );
   }
 }
