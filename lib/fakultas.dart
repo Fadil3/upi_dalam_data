cf7205d8ecc021c904856e380ab3a8b1dbb547c4
Muhammad Rayhan Fadillah <mrayhanfadil@upi.edu>
2022-04-14 22:26:32 +0700

    auto format code

--- lib/fakultas.dart
+++ lib/fakultas.dart
@@ -11,72 +11,23 @@ class _FakultasState extends State<Fakultas> {
   List<Map> listFakultas = [
     {
       "name": "FPMIPA",
-      "url_image":
-          "images/fakultas/FPMIPA.jpg",
+      "url_image": "images/fakultas/FPMIPA.jpg",
     },
-    {
-      "name": "FIP",
-      "url_image":
-          "images/fakultas/FIP.png"
-    },
-    {
-      "name": "FPIPS",
-      "url_image":
-          "images/fakultas/FPIPS.jpg"
-    },
-    {
-      "name": "FPBS",
-      "url_image":
-          "images/fakultas/FPBS.jpg"
-    },
-    {
-      "name": "FPSD",
-      "url_image":
-          "images/fakultas/FPSD.jpg"
-    },
-    {
-      "name": "FPTK",
-      "url_image":
-          "images/fakultas/FPTK.jpg"
-    },
-    {
-      "name": "FPOK",
-      "url_image":
-          "images/fakultas/FPOK.jpg"
-    },
-    {
-      "name": "FPEB",
-      "url_image":
-          "images/fakultas/FPEB.jpeg"
-    },
+    {"name": "FIP", "url_image": "images/fakultas/FIP.png"},
+    {"name": "FPIPS", "url_image": "images/fakultas/FPIPS.jpg"},
+    {"name": "FPBS", "url_image": "images/fakultas/FPBS.jpg"},
+    {"name": "FPSD", "url_image": "images/fakultas/FPSD.jpg"},
+    {"name": "FPTK", "url_image": "images/fakultas/FPTK.jpg"},
+    {"name": "FPOK", "url_image": "images/fakultas/FPOK.jpg"},
+    {"name": "FPEB", "url_image": "images/fakultas/FPEB.jpeg"},
   ];
 
   List<Map> listKamda = [
-    {
-      "name": "Tasikmalaya",
-      "url_image":
-          "images/fakultas/Tasikmalaya.jpg"
-    },
-    {
-      "name": "Cibiru",
-      "url_image":
-          "images/fakultas/Cibiru.jpeg"
-    },
-    {
-      "name": "Sumedang",
-      "url_image":
-          "images/fakultas/Sumedang.jpg"
-    },
-    {
-      "name": "Purwakarta",
-      "url_image":
-          "images/fakultas/Purwakarta.jpg"
-    },
-    {
-      "name": "Serang",
-      "url_image":
-          "images/fakultas/Serang.jpg"
-    }
+    {"name": "Tasikmalaya", "url_image": "images/fakultas/Tasikmalaya.jpg"},
+    {"name": "Cibiru", "url_image": "images/fakultas/Cibiru.jpeg"},
+    {"name": "Sumedang", "url_image": "images/fakultas/Sumedang.jpg"},
+    {"name": "Purwakarta", "url_image": "images/fakultas/Purwakarta.jpg"},
+    {"name": "Serang", "url_image": "images/fakultas/Serang.jpg"}
   ];
   @override
   Widget build(BuildContext context) {
@@ -85,30 +36,37 @@ class _FakultasState extends State<Fakultas> {
         title: const Text("Fakultas"),
       ),
       body: SingleChildScrollView(
-        child: Column(
-          children:[
-          const Padding(padding: EdgeInsets.all(20)),
-          SizedBox(
-            width: 480,
-            child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: const [
-                  Text("Bumi Siliwangi",
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                      )),
-                ]),
-          ),
-          const Padding(padding: EdgeInsets.all(20)),
-          Center(
-              child: SizedBox(
-            width: 480,
-            child: Wrap(spacing: 20.0, runSpacing: 20.0, children: [
-              ...listFakultas.map(
-                (e) => Column(
-                  children: [
-                    Container(
+          child: Column(children: [
+        const Padding(padding: EdgeInsets.all(20)),
+        SizedBox(
+          width: 480,
+          child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: const [
+                Text("Bumi Siliwangi",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    )),
+              ]),
+        ),
+        const Padding(padding: EdgeInsets.all(20)),
+        Center(
+            child: SizedBox(
+          width: 480,
+          child: Wrap(spacing: 20.0, runSpacing: 20.0, children: [
+            ...listFakultas.map(
+              (e) => Column(
+                children: [
+                  InkWell(
+                    splashColor: Colors.blue.withAlpha(30),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => Fakultas()),
+                      );
+                    },
+                    child: Container(
                       width: 100,
                       height: 100,
                       decoration: BoxDecoration(
@@ -119,56 +77,55 @@ class _FakultasState extends State<Fakultas> {
                         ),
                       ),
                     ),
-                    const Padding(padding: EdgeInsets.all(5)),
-                    Text(e["name"]),
-                  ],
-                ),
+                  ),
+                  const Padding(padding: EdgeInsets.all(5)),
+                  Text(e["name"]),
+                ],
               ),
-            ]),
-          )),
-          const Padding(padding: EdgeInsets.all(20)),
-          SizedBox(
-            width: 480,
-            child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: const [
-                  Text("Kampus Daerah",
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                      )),
-                ]),
-          ),
-          const Padding(padding: EdgeInsets.all(20)),
-          Center(
-              child: SizedBox(
-            width: 480,
-            child: Wrap(spacing: 20.0, runSpacing: 20.0, children: [
-              ...listKamda.map(
-                (e) => Column(
-                  children: [
-                    Container(
-                      width: 100,
-                      height: 100,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        image: DecorationImage(
-                          image: AssetImage(e["url_image"]),
-                          fit: BoxFit.cover,
-                        ),
+            ),
+          ]),
+        )),
+        const Padding(padding: EdgeInsets.all(20)),
+        SizedBox(
+          width: 480,
+          child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: const [
+                Text("Kampus Daerah",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    )),
+              ]),
+        ),
+        const Padding(padding: EdgeInsets.all(20)),
+        Center(
+            child: SizedBox(
+          width: 480,
+          child: Wrap(spacing: 20.0, runSpacing: 20.0, children: [
+            ...listKamda.map(
+              (e) => Column(
+                children: [
+                  Container(
+                    width: 100,
+                    height: 100,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      image: DecorationImage(
+                        image: AssetImage(e["url_image"]),
+                        fit: BoxFit.cover,
                       ),
                     ),
-                    const Padding(padding: EdgeInsets.all(5)),
-                    Text(e["name"]),
-                  ],
-                ),
+                  ),
+                  const Padding(padding: EdgeInsets.all(5)),
+                  Text(e["name"]),
+                ],
               ),
-            ]),
-          )),
-          const Padding(padding: EdgeInsets.all(20)),
-          ]
-        )
-      ),
+            ),
+          ]),
+        )),
+        const Padding(padding: EdgeInsets.all(20)),
+      ])),
     );
   }
 }
