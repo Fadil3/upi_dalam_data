e7d9878aac67fe02b1bd8de7a714e2a9f4dc7500
Muhammad Rayhan Fadillah <mrayhanfadil@upi.edu>
2022-04-15 14:43:17 +0700

    delete search bar

--- lib/caridosen.dart
+++ lib/caridosen.dart
@@ -41,109 +41,90 @@ class CariDosenState extends State<CariDosen> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Hello App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Hello'),
-          bottom: AppBar(
-            backgroundColor: Colors.white,
-            title: Container(
-              width: double.infinity,
-              height: 40,
-              color: Colors.white,
-              child: Center(
-                child: TextField(
-                  decoration: InputDecoration(
-                    hintText: 'Masukan Nama Dosen',
-                    prefixIcon: IconButton(
-                      icon: const Icon(Icons.search),
-                      tooltip: 'search',
-                      onPressed: () {
-                        // kalau ditap
-                      },
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Fakultas"),
+      ),
+      body: ListView(children: [
+        Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Row(
+                children: [
+                  Text(
+                    'Urutkan berdasarkan:',
+                    style: const TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        color: Color.fromARGB(255, 32, 30, 30)),
+                  ),
+                  SizedBox(
+                    width: 300,
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        labelText: 'NIM / NIP / NIDN',
+                        border: const OutlineInputBorder(),
+                      ),
                     ),
                   ),
-                ),
+                  DropdownButton(
+                    // Initial Value
+                    value: dropdownvalue,
+
+                    // Down Arrow Icon
+                    icon: const Icon(Icons.keyboard_arrow_down),
+
+                    // Array list of items
+                    items: items.map((String items) {
+                      return DropdownMenuItem(
+                        value: items,
+                        child: Text(items),
+                      );
+                    }).toList(),
+                    onChanged: (String? value) {},
+                  ),
+                ],
               ),
             ),
-          ),
+          ],
         ),
-        body: ListView(children: [
-          Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Row(
-                  children: [
-                    Text(
-                      'Urutkan berdasarkan:',
-                      style: const TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                          color: Color.fromARGB(255, 32, 30, 30)),
-                    ),
-                    DropdownButton(
-                      // Initial Value
-                      value: dropdownvalue,
-
-                      // Down Arrow Icon
-                      icon: const Icon(Icons.keyboard_arrow_down),
-
-                      // Array list of items
-                      items: items.map((String items) {
-                        return DropdownMenuItem(
-                          value: items,
-                          child: Text(items),
-                        );
-                      }).toList(),
-                      onChanged: (String? value) {},
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
-          Container(
-            child: Column(children: [
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        SizedBox(
-                            width: 480,
-                            child: Column(
-                              mainAxisSize: MainAxisSize.min,
-                              children: [
-                                Padding(
-                                    padding: EdgeInsets.all(10),
-                                    child: Card(
-                                      child: Table(
-                                        defaultColumnWidth:
-                                            IntrinsicColumnWidth(),
-                                        defaultVerticalAlignment:
-                                            TableCellVerticalAlignment.middle,
-                                        children: [
-                                          buildRow([
-                                            'Al Tair MT',
-                                            'Guru Besar',
-                                            'PU',
-                                          ]),
-                                        ],
-                                      ),
-                                    )),
-                              ],
-                            )),
-                      ])
-                ],
-              )
-            ]),
-          )
-        ]), //column center
-      ), //Scaffold
-    ); //Material APP
+        Container(
+          child: Column(children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+                  SizedBox(
+                      width: 480,
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Padding(
+                              padding: EdgeInsets.all(10),
+                              child: Card(
+                                child: Table(
+                                  defaultColumnWidth: IntrinsicColumnWidth(),
+                                  defaultVerticalAlignment:
+                                      TableCellVerticalAlignment.middle,
+                                  children: [
+                                    buildRow([
+                                      'Al Tair MT',
+                                      'Guru Besar',
+                                      'PU',
+                                    ]),
+                                  ],
+                                ),
+                              )),
+                        ],
+                      )),
+                ])
+              ],
+            )
+          ]),
+        )
+      ]), //column center
+    );
   }
 }
