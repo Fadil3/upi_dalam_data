25d1262b31ca7dea3c1a678475b438aaae8233a0
bimantoroar <75794187+bimantoroar@users.noreply.github.com>
2022-04-15 13:49:01 +0700

    update mahasiswa.dart
    mengubah button menjadi card

--- lib/mahasiswa.dart
+++ lib/mahasiswa.dart
@@ -11,7 +11,6 @@ class Mahasiswa extends StatefulWidget {
 }
 
 class _MahasiswaState extends State<Mahasiswa> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,21 +18,21 @@ class _MahasiswaState extends State<Mahasiswa> {
         title: const Text('Mahasiswa'),
       ),
       body: Center(
-        child: Column(children: <Widget>[
-          Image.asset(
-            "images/logo-upi.png",
-            height: 50,
-            width: 50,
-          ),
-          SizedBox(
-            width: 300,
-            child: Column(children: [
-              Card(
+        child: SingleChildScrollView(
+          child: Column(children: <Widget>[
+            Image.asset(
+              "images/logo-upi.png",
+              height: 50,
+              width: 50,
+            ),
+            SizedBox(
+              width: 350,
+              child: Column(children: [
+                Card(
                   elevation: 2,
                   child: Column(
                     children: const [
-                      Text(
-                          "Jumlah Mahasiswa Universitas Pendidikan Indonesia",
+                      Text("Jumlah Mahasiswa Universitas Pendidikan Indonesia",
                           textAlign: TextAlign.center),
                       Text(
                         '41.633',
@@ -43,65 +42,146 @@ class _MahasiswaState extends State<Mahasiswa> {
                             color: Colors.black),
                       ),
                     ],
-                  )),
-            ]),
-          ),
-          const Padding(
-            padding: EdgeInsets.all(20),
-          ),
-          SizedBox(
-            width: 300,
-            height: 35,
-            child: ElevatedButton(
-              onPressed: () {
-                // Respond to button press
-              },
-              child: const Text('DATA MAHASISWA BELUM BAYAR UKT'),
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                ),
+              ]),
             ),
-          ),
-          const Padding(
-            padding: EdgeInsets.all(20),
-          ),
-          SizedBox(
-            width: 300,
-            height: 35,
-            child: ElevatedButton(
-              onPressed: () {
-                // Respond to button press
-              },
-              child: const Text('RATA-RATA LAMA STUDI'),
+            const Padding(
+              padding: EdgeInsets.all(20),
             ),
-          ),
-          const Padding(
-            padding: EdgeInsets.all(20),
-          ),
-          SizedBox(
-            width: 300,
-            height: 35,
-            child: ElevatedButton(
-              onPressed: () {
-                // Respond to button press
-              },
-              child: const Text('MAHASISWA PROGRAM DI LUAR KAMPUS'),
+            SizedBox(
+              width: 350,
+              child: InkWell(
+                splashColor: Colors.blue.withAlpha(30),
+                onTap: () {},
+                child: Card(
+                  semanticContainer: true,
+                  clipBehavior: Clip.antiAliasWithSaveLayer,
+                  child: Column(
+                    children: [
+                      ListTile(
+                        title: const Text('Data Belum Bayar UKT'),
+                        subtitle: Text(
+                          'Jumlah Belum Bayar UKT:                        52 Mahasiswa',
+                          style:
+                              TextStyle(color: Colors.black.withOpacity(0.6)),
+                        ),
+                        isThreeLine: true,
+                        leading: const Icon(Icons.supervisor_account_outlined),
+                      ),
+                    ],
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  elevation: 5,
+                ),
+              ),
             ),
-          ),
-          const Padding(
-            padding: EdgeInsets.all(20),
-          ),
-          SizedBox(
-            width: 300,
-            height: 35,
-            child: ElevatedButton(
-              onPressed: () {
-                // Respond to button press
-              },
-              child: const Text('DATA PENGAJUAN BEASISWA'),
+            const Padding(
+              padding: EdgeInsets.all(10),
             ),
-          ),
-          const Padding(
-            padding: EdgeInsets.all(20),
-          ),
-        ]),
+            SizedBox(
+              width: 350,
+              child: InkWell(
+                splashColor: Colors.blue.withAlpha(30),
+                onTap: () {},
+                child: Card(
+                  semanticContainer: true,
+                  clipBehavior: Clip.antiAliasWithSaveLayer,
+                  child: Column(
+                    children: [
+                      ListTile(
+                        title: const Text('Rata-rata Lama Studi'),
+                        subtitle: Text(
+                          'Rata-rata Lama Studi Mahasiswa:                4 Tahun',
+                          style:
+                              TextStyle(color: Colors.black.withOpacity(0.6)),
+                        ),
+                        isThreeLine: true,
+                        leading: const Icon(Icons.supervisor_account_outlined),
+                      ),
+                    ],
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  elevation: 5,
+                ),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.all(10),
+            ),
+            SizedBox(
+              width: 350,
+              child: InkWell(
+                splashColor: Colors.blue.withAlpha(30),
+                onTap: () {},
+                child: Card(
+                  semanticContainer: true,
+                  clipBehavior: Clip.antiAliasWithSaveLayer,
+                  child: Column(
+                    children: [
+                      ListTile(
+                        title: const Text('Mahasiswa Program di Luar Kampus'),
+                        subtitle: Text(
+                          'Jumlah Mahasiswa Program di Luar Kampus: 20 Mahasiswa',
+                          style:
+                              TextStyle(color: Colors.black.withOpacity(0.6)),
+                        ),
+                        leading: const Icon(Icons.supervisor_account_outlined),
+                        isThreeLine: true,
+                      ),
+                    ],
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  elevation: 5,
+                ),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.all(10),
+            ),
+            SizedBox(
+              width: 350,
+              child: InkWell(
+                splashColor: Colors.blue.withAlpha(30),
+                onTap: () {},
+                child: Card(
+                  semanticContainer: true,
+                  clipBehavior: Clip.antiAliasWithSaveLayer,
+                  child: Column(
+                    children: [
+                      ListTile(
+                        title: const Text('Data Pengajuan Beasiswa'),
+                        subtitle: Text(
+                          'Jumlah Mahasiswa yang Mengajukan Beasiswa: 15 Mahasiswa',
+                          style:
+                              TextStyle(color: Colors.black.withOpacity(0.6)),
+                        ),
+                        isThreeLine: true,
+                        leading: const Icon(Icons.supervisor_account_outlined),
+                      ),
+                    ],
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  elevation: 5,
+                ),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.all(20),
+            ),
+          ]),
+        ),
       ),
     );
   }
