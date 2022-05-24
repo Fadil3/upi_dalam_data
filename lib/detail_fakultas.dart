1494ba59743d0c2b44b253abe19d1fc7b24ffcc7
Muhammad Rayhan Fadillah <mrayhanfadil@upi.edu>
2022-05-24 16:34:38 +0700

    create keketatan chart

--- lib/detail_fakultas.dart
+++ lib/detail_fakultas.dart
@@ -80,6 +80,21 @@ class _DetailFakultasState extends State<DetailFakultas> {
     {"name": "FPOK", "url_image": "images/fakultas/FPOK.jpg"},
     {"name": "FPEB", "url_image": "images/fakultas/FPEB.jpeg"},
   ];
+  List<_ChartData>? chartData;
+
+  @override
+  void initState() {
+    chartData = <_ChartData>[
+      _ChartData(2017, 18, 17, 7),
+      _ChartData(2018, 29, 29, 11),
+      _ChartData(2019, 18, 17, 7),
+      _ChartData(2020, 18, 11, 7),
+      _ChartData(2021, 20, 12, 8),
+      _ChartData(2022, 18, 10, 4),
+    ];
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     Map data = infoFakultas
@@ -192,6 +207,23 @@ class _DetailFakultasState extends State<DetailFakultas> {
                 fontWeight: FontWeight.bold,
               ),
             ),
+            SfCartesianChart(
+              plotAreaBorderWidth: 0,
+              title: ChartTitle(text: 'Keketatan Penerimaan Mahasiswa'),
+              legend: Legend(
+                  isVisible: true, overflowMode: LegendItemOverflowMode.wrap),
+              primaryXAxis: NumericAxis(
+                  edgeLabelPlacement: EdgeLabelPlacement.shift,
+                  interval: 2,
+                  majorGridLines: const MajorGridLines(width: 0)),
+              primaryYAxis: NumericAxis(
+                  labelFormat: '{value}',
+                  axisLine: const AxisLine(width: 0),
+                  majorTickLines:
+                      const MajorTickLines(color: Colors.transparent)),
+              series: _getDefaultLineSeries(),
+              tooltipBehavior: TooltipBehavior(enable: true),
+            ),
             const Padding(padding: EdgeInsets.all(5)),
             const Text(
               "Program Studi",
@@ -218,4 +250,42 @@ class _DetailFakultasState extends State<DetailFakultas> {
       ))),
     );
   }
+
+  /// The method returns line series to chart.
+  List<LineSeries<_ChartData, num>> _getDefaultLineSeries() {
+    return <LineSeries<_ChartData, num>>[
+      LineSeries<_ChartData, num>(
+          animationDuration: 2500,
+          dataSource: chartData!,
+          xValueMapper: (_ChartData sales, _) => sales.x,
+          yValueMapper: (_ChartData sales, _) => sales.y,
+          width: 2,
+          name: 'SNMPTN',
+          markerSettings: const MarkerSettings(isVisible: true)),
+      LineSeries<_ChartData, num>(
+          animationDuration: 2500,
+          dataSource: chartData!,
+          width: 2,
+          name: 'SBMPTN',
+          xValueMapper: (_ChartData sales, _) => sales.x,
+          yValueMapper: (_ChartData sales, _) => sales.y2,
+          markerSettings: const MarkerSettings(isVisible: true)),
+      LineSeries<_ChartData, num>(
+          animationDuration: 2500,
+          dataSource: chartData!,
+          width: 2,
+          name: 'SMUPI',
+          xValueMapper: (_ChartData sales, _) => sales.x,
+          yValueMapper: (_ChartData sales, _) => sales.y3,
+          markerSettings: const MarkerSettings(isVisible: true))
+    ];
+  }
+}
+
+class _ChartData {
+  _ChartData(this.x, this.y, this.y2, this.y3);
+  final double x;
+  final double y;
+  final double y2;
+  final double y3;
 }
